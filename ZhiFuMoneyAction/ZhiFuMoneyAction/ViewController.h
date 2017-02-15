253ed4d0839e807d04f8b1e47c209409beeb8c06
huyiyong <991779153@qq.com>
2017-02-15 22:47:58 +0800

    创建支付文件

---
+++ ZhiFuMoneyAction/ZhiFuMoneyAction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhiFuMoneyAction
+//
+//  Created by huyiyong on 17/2/15.
+//  Copyright © 2017年 huyiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZhiFuMoneyAction/ZhiFuMoneyAction/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZhiFuMoneyAction
+//
+//  Created by huyiyong on 17/2/15.
+//  Copyright © 2017年 huyiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
